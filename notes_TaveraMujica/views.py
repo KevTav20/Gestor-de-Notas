daf23256248d06fcfb23633a68d86dee8164c9a5
KevTav20 <kevin_tavera11@yahoo.com>
2024-10-20 20:13:44 -0600

    Start to create: Templates, urls and views

--- notes_TaveraMujica/views.py
+++ notes_TaveraMujica/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from lib2to3.fixes.fix_input import context
+
+from django.shortcuts import render, get_object_or_404
+from .models import User
 
 # Create your views here.
+from .models import User, Note
+
+def index(request):
+
+    return render(request, "/notes/index.html", context)
+
+def list(request, user_id):
+    user = get_object_or_404(User, pk = user_id)
+    return render(request, "/notes/list.html", context)
